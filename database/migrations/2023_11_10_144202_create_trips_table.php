92a64dbefd4a6d574d681a6df9aab815dbb622ba
Zarko Gavric <zarko.gavric@sportion.net>
2023-11-15 10:08:31 +0100

    Finished setting up backend and database for app

--- database/migrations/2023_11_10_144202_create_trips_table.php
+++ database/migrations/2023_11_10_144202_create_trips_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Driver;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('trips', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(User::class);
+            $table->foreignId(Driver::class);
+            $table->boolean('is_started')->default(false);
+            $table->boolean('is_complete')->default(false);
+            $table->json('origin')->nullable();
+            $table->json('destination')->nullable();
+            $table->string('destination_name')->nullable();
+            $table->json('driver_location')->nullable();
             $table->timestamps();
         });
     }
